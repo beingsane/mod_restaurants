b6e3f68a8a5937075ac860d9494b3f5f0a010ee1
Marc Bowman <marcmonk@gmail.com>
2014-09-08 22:59:15 -0400

    debugging link hover

--- tmpl/default.php
+++ tmpl/default.php
@@ -21,7 +21,7 @@ defined('_JEXEC') or die;
 				<!--start the new accordion group for this neighborhood-->
 				<div class="accordion-group">
 					<div class="accordion-heading">
-						<a class="accordion-toggle neighborhood" style="background-color: #<?php echo $item->bgcolor ?>; color: #<?php echo $item->color ?>;" href="#collapse<?php echo $item->bgcolor ?>" data-toggle="collapse" data-parent="#neighborhoods-acc"> <?php echo $item->neighborhood ?> </a>
+						<a class="accordion-toggle neighborhood-<?php echo $item->color ?>" style="background-color: #<?php echo $item->bgcolor ?>;" href="#collapse<?php echo $item->bgcolor ?>" data-toggle="collapse" data-parent="#neighborhoods-acc"> <?php echo $item->neighborhood ?> </a>
 					</div>
 					<div id="collapse<?php echo $item->bgcolor ?>" class="accordion-body collapse">
 						<div class="accordion-inner">
@@ -32,8 +32,8 @@ defined('_JEXEC') or die;
 					todo: bottom-dotted
 					todo: blank/missing fields
 					todo: acute e (as in cafe)
-					todo: hover effects
-					todo: slide close/slide open
+					done: hover effects
+					done: slide close/slide open
 				-->
 				<div class="span2 restaurant-name<?php if($item->display_logo == 1): ?> display-logo<?php endif; ?>"><?php echo $item->restaurant ?></div>
 				<div class="span2"><a href="<?php echo $item->website; ?>" target="_blank"><?php echo $item->website; ?></a><br /> <?php echo $item->address1; ?></div>
