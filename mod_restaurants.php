b64e976924f4b392d9925e3f70dd25fe4a2e9942
v3gan <marcmonk@gmail.com>
2014-10-19 12:16:23 -0400

    adding module level css and js folers and files

--- mod_restaurants.php
+++ mod_restaurants.php
@@ -3,6 +3,8 @@ defined('_JEXEC') or die;
 
 require_once __DIR__ . '/helper.php';
 
+mod_restaurantsHelper::getHead($params,$module);
 $list = mod_restaurantsHelper::getList($params);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
 require JModuleHelper::getLayoutPath('mod_restaurants', $params->get('layout', 'default'));
